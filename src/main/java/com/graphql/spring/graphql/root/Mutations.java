7e6faa129c30589240c2667047db2317930c6a4b
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2017-07-25 15:54:39 +0200

    exercise 3

--- src/main/java/com/graphql/spring/graphql/root/Mutations.java
+++ src/main/java/com/graphql/spring/graphql/root/Mutations.java
@@ -1,6 +1,7 @@
 package com.graphql.spring.graphql.root;
 
 import com.graphql.spring.jpa.Product;
+import com.graphql.spring.jpa.Store;
 import com.graphql.spring.jpa.repositories.ProductRepository;
 import com.graphql.spring.jpa.repositories.StoreRepository;
 import graphql.annotations.GraphQLField;
@@ -24,5 +25,12 @@ public class Mutations {
         product.setPrice(input.price);
         product.setStore(storeRepository.findOne(input.storeId));
         return productRepository.save(product);
+    }
+
+    @GraphQLField
+    public Store addStore(@GraphQLName("name") String name) {
+        Store store = new Store();
+        store.setName(name);
+        return storeRepository.save(store);
     }
 }
